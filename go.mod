eab0d17dca913bb88c3cf5bfa6dcba878de02aa2
noriah reuland <code@noriah.dev>
2020-09-25 17:58:06 -0700

    hahahahah
    nora fixed the audio issue.
    figures. she basically breathes sound (heheh, get it?)

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/noriah/tavis
 
 go 1.15
-
-require github.com/gordonklaus/portaudio v0.0.0-20200911161147-bb74aa485641
--- input/portaudio.go
+++ input/portaudio.go
@@ -5,15 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-	"time"
 	"unsafe"
 
-	"github.com/gordonklaus/portaudio"
+	"github.com/noriah/tavis/input/portaudio"
 )
 
 // errors
 var (
-	ErrBadDevice error = errors.New("device not found")
+	ErrBadDevice    error = errors.New("device not found")
+	ErrReadTimedOut error = errors.New("read timed out")
 )
 
 // SampleType is the datatype we want from our inputs
@@ -32,14 +32,14 @@ func (sb SampleBuffer) Ptr() unsafe.Pointer {
 // The number of frames per read will be
 type Portaudio struct {
 	stream        *portaudio.Stream // our input stream
-	scratchBuffer []SampleType      // internal scratch buffer
 	loopChanBytes chan int
 	loopChanCmplx chan SampleBuffer
 
-	DeviceName string  // name of device to look for
-	FrameSize  int     // number of channels per frame
-	SampleSize int     // number of frames per buffer write
-	SampleRate float64 // sample rate
+	DeviceName   string       // name of device to look for
+	FrameSize    int          // number of channels per frame
+	SampleSize   int          // number of frames per buffer write
+	SampleRate   float64      // sample rate
+	SampleBuffer SampleBuffer // internal scratch buffer
 }
 
 // Init sets up all the portaudio things we need to do
@@ -79,32 +79,32 @@ func (pa *Portaudio) Init() error {
 			SampleRate:      pa.SampleRate,
 			FramesPerBuffer: pa.SampleSize,
 			Flags:           portaudio.ClipOff | portaudio.DitherOff,
-		}, pa.PaCallback); err != nil {
+		}, pa.SampleBuffer); err != nil {
 		return err
 	}
-
-	pa.loopChanCmplx = make(chan SampleBuffer, 1)
-	pa.loopChanBytes = make(chan int)
 
 	return err
 }
 
-func (pa *Portaudio) Read(ctx context.Context, buf SampleBuffer) int {
-	select {
-	case pa.loopChanCmplx <- buf:
+// ReadyRead returns the number of frames ready to read
+func (pa *Portaudio) ReadyRead() int {
+	var ready, _ = pa.stream.AvailableToRead()
+	return ready
+}
+
+// Read signals portaudio to dump some data into the buffer we gave it.
+// Will block if there is not enough data yet.
+func (pa *Portaudio) Read(ctx context.Context) error {
+	for pa.ReadyRead() < pa.SampleSize {
 		select {
-		case dst := <-pa.loopChanBytes:
-			return dst
-
 		case <-ctx.Done():
-			fmt.Println("ctx.Done signal - on return")
+			fmt.Println("read timed out")
+			return ErrReadTimedOut
+		default:
 		}
-
-	case <-ctx.Done():
-		fmt.Println("ctx.Done signal - on recieve")
 	}
 
-	return 0
+	return pa.stream.Read()
 }
 
 // Close closes the close close
@@ -112,27 +112,27 @@ func (pa *Portaudio) Close() error {
 	return pa.stream.Close()
 }
 
-// PaCallback does a call to the back for stock check
-func (pa *Portaudio) PaCallback(in SampleBuffer,
-	timeInfo portaudio.StreamCallbackTimeInfo,
-	flags portaudio.StreamCallbackFlags) {
+// // PaCallback does a call to the back for stock check
+// func (pa *Portaudio) PaCallback(in SampleBuffer,
+// 	timeInfo portaudio.StreamCallbackTimeInfo,
+// 	flags portaudio.StreamCallbackFlags) {
 
-	select {
-	case dst := <-pa.loopChanCmplx:
+// 	select {
+// 	case dst := <-pa.loopChanCmplx:
 
-		var idx int
-		for idx = 0; idx < len(in) && idx < len(dst); idx++ {
-			dst[idx] = in[idx]
-		}
+// 		var idx int
+// 		for idx = 0; idx < len(in) && idx < len(dst); idx++ {
+// 			dst[idx] = in[idx]
+// 		}
 
-		select {
-		case pa.loopChanBytes <- idx:
-		case <-time.After(time.Second / 2):
-			fmt.Println("response send timed out")
-		}
-	default:
-	}
-}
+// 		select {
+// 		case pa.loopChanBytes <- idx:
+// 		case <-time.After(time.Second / 2):
+// 			fmt.Println("response send timed out")
+// 		}
+// 	default:
+// 	}
+// }
 
 // Start will open the stream and start audio processing
 func (pa *Portaudio) Start() {
