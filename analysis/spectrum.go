450563157dc1cfe3030632d7c26c0439da7e206d
noriah reuland <code@noriah.dev>
2020-09-28 21:19:39 -0700

    Now updates on size change

--- analysis/spectrum.go
+++ analysis/spectrum.go
@@ -12,6 +12,8 @@ import (
 const (
 	AutoScalingSeconds = 10
 
+	AutoScalingDumpPercent = 0.75
+
 	MaxBars = 1024
 )
 
@@ -32,7 +34,10 @@ type Spectrum struct {
 	// frameSize is the number of channels we expect per frame
 	frameSize int
 
-	workBuffer []float64
+	// winMax is the maximum number of values in our sliding window
+	winMax int
+
+	workBins []float64
 
 	heightWindow []*util.MovingWindow
 	peakHeight   []float64
@@ -61,15 +66,15 @@ func (s *Spectrum) init() error {
 
 	s.maxBars = MaxBars
 
-	s.workBuffer = make([]float64, s.maxBars*s.frameSize)
+	s.workBins = make([]float64, s.maxBars*s.frameSize)
 
-	winMax := int((AutoScalingSeconds * s.sampleRate) / float64(s.sampleSize))
+	s.winMax = int((AutoScalingSeconds*s.sampleRate)/float64(s.sampleSize)) * 2
 
 	s.heightWindow = make([]*util.MovingWindow, s.frameSize)
 	s.peakHeight = make([]float64, s.frameSize)
 
 	for idx := 0; idx < s.frameSize; idx++ {
-		s.heightWindow[idx] = util.NewMovingWindow(winMax)
+		s.heightWindow[idx] = util.NewMovingWindow(s.winMax)
 	}
 
 	s.loCuts = make([]binType, s.maxBars+1)
@@ -89,7 +94,7 @@ func (s *Spectrum) Print() {
 // Bins returns a slice of only the bins we are expecting to process
 func (s *Spectrum) Bins() []float64 {
 	var bars int = s.numBins * s.frameSize
-	return s.workBuffer[:bars:bars]
+	return s.workBins[:bars:bars]
 }
 
 // Recalculate rebuilds our frequency bins with bars bin counts
@@ -154,7 +159,7 @@ func (s *Spectrum) Generate(buf fftw.CmplxBuffer) {
 	bl = len(buf) / s.frameSize
 
 	for xChn = 0; xChn < s.frameSize; xChn++ {
-		s.peakHeight[xChn] = 1
+		s.peakHeight[xChn] = 0.125
 	}
 
 	for xChn = 0; xChn < s.frameSize; xChn++ {
@@ -173,10 +178,10 @@ func (s *Spectrum) Generate(buf fftw.CmplxBuffer) {
 			barMag = barMag / float64(s.hiCuts[xBin]-s.loCuts[xBin]+1)
 			barMag = barMag * boost
 
-			s.workBuffer[xBuf] = math.Pow(barMag, 0.5)
+			s.workBins[xBuf] = math.Pow(barMag, 0.5)
 
-			if s.workBuffer[xBuf] > s.peakHeight[xChn] {
-				s.peakHeight[xChn] = s.workBuffer[xBuf]
+			if s.workBins[xBuf] > s.peakHeight[xChn] {
+				s.peakHeight[xChn] = s.workBins[xBuf]
 			}
 		}
 	}
@@ -192,7 +197,7 @@ func (s *Spectrum) Scale(height int) {
 		xBin int // bin index
 		xChn int // channel index
 
-		cHeight float64 // window half height constant
+		cHeight float64 // height constant
 
 		vMag  float64 // magnitude variable
 		vMean float64 // average variable
@@ -202,12 +207,16 @@ func (s *Spectrum) Scale(height int) {
 	cHeight = float64(height)
 
 	for xChn = 0; xChn < s.frameSize; xChn++ {
+		if s.heightWindow[xChn].Size() >= s.winMax {
+			s.heightWindow[xChn].Drop(int(float64(s.winMax) * AutoScalingDumpPercent))
+		}
+
 		vMean, vSD = s.heightWindow[xChn].Update(s.peakHeight[xChn])
 
 		vMag = math.Max(vMean+(2*vSD), 1.0)
 
-		for xBin = xChn; xBin < s.numBins*s.frameSize+xChn; xBin += s.frameSize {
-			s.workBuffer[xBin] = math.Min(cHeight-1, ((s.workBuffer[xBin]/vMag)*cHeight)-1)
+		for xBin = xChn; xBin < s.numBins*s.frameSize; xBin += s.frameSize {
+			s.workBins[xBin] = math.Min(cHeight-1, ((s.workBins[xBin]/vMag)*cHeight)-1)
 		}
 	}
 }
@@ -216,51 +225,52 @@ func (s *Spectrum) Monstercat(factor float64) {
 
 	var (
 		xBin int
-		xChn int
 		pass int
-		xBuf int
 		tmp  float64
 	)
 
-	for xChn = 0; xChn < s.frameSize; xChn++ {
-
-		for xBin = 0; xBin < s.numBins; xBin++ {
-
-			if xBin > 0 {
-				xBuf = (xBin * s.frameSize) + xChn
-				for pass = xBin - 1; pass >= 0; pass-- {
-					tmp = s.workBuffer[xBuf] / math.Pow(factor, float64(xBin-pass))
-					if tmp > s.workBuffer[xBuf] {
-						s.workBuffer[xBuf] = tmp
-					}
+	for xBin = 0; xBin < s.numBins*s.frameSize; xBin++ {
+		if xBin > 0 {
+			for pass = xBin - 1; pass >= 0; pass-- {
+				tmp = s.workBins[xBin] / math.Pow(factor, float64(xBin-pass))
+				if tmp > s.workBins[xBin] {
+					s.workBins[xBin] = tmp
 				}
+			}
 
-				for pass = xBin + 1; pass < s.numBins; pass++ {
-					tmp = s.workBuffer[xBuf] / math.Pow(factor, float64(pass-xBin))
-					if tmp > s.workBuffer[xBuf] {
-						s.workBuffer[xBuf] = tmp
-					}
+			for pass = xBin + 1; pass < s.numBins; pass++ {
+				tmp = s.workBins[xBin] / math.Pow(factor, float64(pass-xBin))
+				if tmp > s.workBins[xBin] {
+					s.workBins[xBin] = tmp
 				}
 			}
 		}
 	}
 }
 
-// func (s *Spectrum)
+func (s *Spectrum) Falloff(weight float64) {
+	var (
+		xBin int
+		vMag float64
+	)
 
-// if xBin > 0 {
+	for xBin = 0; xBin < s.numBins; xBin++ {
+		vMag = math.Min(s.workBins[xBin]*weight, s.workBins[xBin]-1)
+		s.workBins[xBin] = math.Max(vMag, s.workBins[xBin])
+	}
+}
 
 // 	for pass = xBin - 1; pass >= 0; pass-- {
-// 		tmp = s.workBuffer[xBuf] / math.Pow(factor, float64(xBin-pass))
-// 		if tmp > s.workBuffer[xBuf] {
-// 			s.workBuffer[xBuf] = tmp
+// 		tmp = s.workBins[xBuf] / math.Pow(factor, float64(xBin-pass))
+// 		if tmp > s.workBins[xBuf] {
+// 			s.workBins[xBuf] = tmp
 // 		}
 // 	}
 
 // 	for pass = xBin + 1; pass < s.numBins; pass++ {
-// 		tmp = s.workBuffer[xBuf] / math.Pow(factor, float64(pass-xBin))
-// 		if tmp > s.workBuffer[xBuf] {
-// 			s.workBuffer[xBuf] = tmp
+// 		tmp = s.workBins[xBuf] / math.Pow(factor, float64(pass-xBin))
+// 		if tmp > s.workBins[xBuf] {
+// 			s.workBins[xBuf] = tmp
 // 		}
 // 	}
 // }
@@ -274,7 +284,7 @@ func (s *Spectrum) Monstercat(factor float64) {
 // 	for xBin = 0; xBin < s.numBins; xBin++ {
 // 		xBuf = (xBin * s.frameSize) + xChn
 
-// 		tmp = (s.workBuffer[xBuf] / s.peakHeight[xChn]) * boost
+// 		tmp = (s.workBins[xBuf] / s.peakHeight[xChn]) * boost
 
 // 		s.barBuffer[xBuf] = BarType(tmp)
 
