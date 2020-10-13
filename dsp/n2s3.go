64982a6c17df9d05243b2642ccc0582e350df8ae
noriah <vix@noriah.dev>
2020-10-12 21:46:33 -0700

    nora - so i just cant be happy with anything

--- dsp/n2s3.go
+++ dsp/n2s3.go
@@ -15,8 +15,8 @@ type N2S3State struct {
 
 // NewN2S3State returns a new N2S3 state.
 func NewN2S3State(hz float64, samples int, max int) *N2S3State {
-	slowMax := int((5*hz)/float64(samples)) * 2
-	fastMax := int((1*hz)/float64(samples)) * 2
+	slowMax := int((6*hz)/float64(samples)) * 2
+	fastMax := int((2*hz)/float64(samples)) * 2
 
 	return &N2S3State{
 		prevBins:   make([]float64, max),
@@ -28,20 +28,31 @@ func NewN2S3State(hz float64, samples int, max int) *N2S3State {
 // N2S3 does nora's not so special smoothing
 func N2S3(bins []float64, count int, height float64, state *N2S3State) {
 
-	var xBin = 0
+	var sum = 0.0
+	var max = 0.0
+	for xBin := 0; xBin < count; xBin++ {
+
+		if state.prevBins[xBin] > max {
+			max = state.prevBins[xBin]
+		}
+
+		sum += state.prevBins[xBin]
+	}
+
+	var avg = sum / float64(count)
+
 	var peak = 0.0
 
-	for xBin < count {
+	for xBin := 0; xBin < count; xBin++ {
 
 		state.prevBins[xBin] = math.Max(0,
-			n2s3Delta(bins[xBin], state.prevBins[xBin]))
+			n2s3Next(bins[xBin], state.prevBins[xBin], avg))
+
 		bins[xBin] = state.prevBins[xBin]
 
 		if peak < bins[xBin] {
 			peak = bins[xBin]
 		}
-
-		xBin++
 	}
 
 	if peak <= 0 {
@@ -56,42 +67,39 @@ func N2S3(bins []float64, count int, height float64, state *N2S3State) {
 	if length := state.slowWindow.Len(); length >= state.fastWindow.Cap() {
 
 		if math.Abs(state.fastWindow.Mean()-vMean) > (0.9 * vSD) {
-			vMean, vSD = state.slowWindow.Drop(int(float64(length) * 0.75))
+			vMean, vSD = state.slowWindow.Drop(int(float64(length) * 0.65))
 		}
 	}
 
 	// value to scale by to make conditions easier to base on
 	var scale = height / math.Max(vMean+(1.5*vSD), math.SmallestNonzeroFloat64)
 
-	xBin = 0
-	for xBin < count {
+	for xBin := 0; xBin < count; xBin++ {
 		bins[xBin] = math.Min(height, bins[xBin]*scale)
-		xBin++
 	}
 }
 
 //
 // SUBJECT TO CHANGE!!!
 //
-// n2s3Delta provided with a real and previous value will return the
+// n2s3Next provided with a real and previous value will return the
 // delta to add to the previous value.
-func n2s3Delta(real, prev float64) float64 {
+func n2s3Next(real, prev, avg float64) float64 {
+	if real == 0 {
+
+		return prev * 0.5
+	}
 
 	var d = real - prev
 
-	// if the real target is below our current value
-	if d < 0.0 {
-
-		if d > -0.5 {
-			return prev - (d * d)
-		}
-
-		return prev + (d * 0.62)
+	if ad := math.Abs(d); ad < 1 {
+		ad /= 2
+		return prev + (d * ad)
 	}
 
-	if d < 0.5 {
-		return prev + (d * d)
+	if d > 0.0 {
+		return prev + math.Min(d, math.Min(d*(d/real), prev+0.1))
 	}
 
-	return prev + (d * 0.7)
+	return math.Min(real, prev-(1+(d/prev)))
 }
