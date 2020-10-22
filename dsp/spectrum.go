a70b06a2d4fbbd69c7abd471e3e5ed55918b0c90
noriah <vix@noriah.dev>
2020-10-21 23:30:04 -0700

    Well something works?

--- dsp/spectrum.go
+++ dsp/spectrum.go
@@ -26,7 +26,6 @@ func (bs *BinSet) Len() int {
 
 // Spectrum is an audio spectrum in a buffer
 type Spectrum struct {
-	maxBins int
 	numBins int
 
 	fftSize int
@@ -50,7 +49,6 @@ func NewSpectrum(hz float64, size int) *Spectrum {
 	var fftSize = (size / 2) + 1
 
 	var sp = &Spectrum{
-		maxBins:    size,
 		numBins:    size,
 		fftSize:    fftSize,
 		fftBuf:     make([]complex128, fftSize),
@@ -69,60 +67,9 @@ func NewSpectrum(hz float64, size int) *Spectrum {
 // BinSet reurns a new data set with settings matching this spectrum
 func (sp *Spectrum) BinSet(input []float64) *BinSet {
 	return &BinSet{
-		buffer: make([]float64, sp.maxBins),
+		buffer: make([]float64, sp.sampleSize),
 		plan:   fft.NewPlan(input, sp.fftBuf),
 	}
-}
-
-// Recalculate rebuilds our frequency bins with bins bin counts
-func (sp *Spectrum) Recalculate(bins int) int {
-	if bins > sp.maxBins {
-		bins = sp.maxBins
-	}
-
-	sp.numBins = bins
-
-	var cBins = float64(bins + 1)
-
-	var cHiNyquist = ((float64(sp.sampleSize) / 4) / (sp.sampleRate / 2))
-
-	var cCoef = 100.0 / float64(bins)
-
-	var cF = math.Log10(1/(sp.sampleRate/2)) / ((1 / cBins) - 1)
-
-	// so this came from dpayne/cli-visualizer
-	// until i can find a different solution
-	for xB := 1; xB <= bins; xB++ {
-		var fxB = float64(xB)
-		// Fix issue where recalculations may not be accurate due to
-		// previous recalculations
-		sp.loCuts[xB] = 0
-		sp.hiCuts[xB] = 0
-
-		var vFreq = (fxB / (cBins * cF)) - cF
-		vFreq = math.Pow(10.0, vFreq) * cHiNyquist
-
-		sp.loCuts[xB] = int(vFreq)
-
-		if xB > 0 {
-			if sp.loCuts[xB] <= sp.loCuts[xB-1] {
-				sp.loCuts[xB] = sp.loCuts[xB-1] + 1
-			}
-
-			// previous high cutoffs are equal to previous low cuttoffs?
-			sp.hiCuts[xB-1] = sp.loCuts[xB-1]
-
-			if sp.hiCuts[xB-1] >= sp.fftSize {
-				sp.hiCuts[xB-1] = sp.fftSize - 1
-			}
-
-			var diff = sp.hiCuts[xB-1] - sp.loCuts[xB-1] + 1
-
-			sp.eqBins[xB-1] = (math.Log2(fxB+2) / float64(diff)) * cCoef
-		}
-	}
-
-	return bins
 }
 
 // Generate makes numBins and dumps them in the buffer
@@ -135,10 +82,86 @@ func (sp *Spectrum) Generate(bs *BinSet) {
 	for xB := 0; xB < sp.numBins; xB++ {
 		var mag = 0.0
 
-		for xF := sp.loCuts[xB]; xF <= sp.hiCuts[xB] && xF >= 0; xF++ {
+		for xF := sp.loCuts[xB]; xF < sp.hiCuts[xB] && xF >= 0; xF++ {
 			mag += cmplx.Abs(sp.fftBuf[xF])
 		}
 
 		bs.buffer[xB] = math.Pow(mag*sp.eqBins[xB], 0.5)
+		// bs.buffer[xB] = math.Pow(mag, 0.5)
 	}
+}
+
+// Frequencies [0] -- Bass -- [1] -- Mid -- [2] -- Treble -- [3]
+var dividers = []float64{
+	20.0,
+	150.0,
+	3600.0,
+	16000.0,
+}
+
+// Recalculate rebuilds our frequency bins
+//
+// https://stackoverflow.com/questions/3694918/how-to-extract-frequency-associated-with-fft-values-in-python
+//  - https://stackoverflow.com/a/27191172
+// https://www.cg.tuwien.ac.at/courses/WissArbeiten/WS2010/processing.pdf
+func (sp *Spectrum) Recalculate(bins int) int {
+	if bins > sp.sampleSize {
+		bins = sp.sampleSize
+	}
+
+	sp.numBins = bins
+
+	// var dt = 1 / (sp.sampleRate / float64(sp.sampleSize))
+
+	// var T = dt * float64(sp.fftSize)
+
+	// var df = 1 / T
+
+	// var dw = 2 * math.Pi / T
+
+	// var ny = dw * sampleSize / 2
+
+	// - RATE: 44100 | SIZE: 1024
+	// - MAX: 512
+	// - PASS inside the array and where expected
+	// var max = int(math.Floor(22050.0 * T))
+	// panic(max)
+
+	// var cCoef = 100.0 / float64(bins)
+
+	var lo = dividers[0]
+	var hi = dividers[3]
+
+	var cF = math.Log10(lo/hi) / (1/float64(bins+1) - 1)
+
+	for xB := 0; xB <= bins; xB++ {
+		// Fix issue where recalculations may not be accurate due to
+		// previous recalculations
+		sp.loCuts[xB] = 0
+		sp.hiCuts[xB] = 0
+
+		var vFreq = (((float64(xB+1) / float64(bins+1)) * cF) - cF)
+		vFreq = (math.Pow(10.0, vFreq) * hi) / (sp.sampleRate / 2)
+		vFreq *= float64(sp.sampleSize / 4)
+
+		sp.loCuts[xB] = int(math.Floor(vFreq))
+
+		if xB > 0 {
+			if sp.loCuts[xB] <= sp.loCuts[xB-1] {
+				sp.loCuts[xB] = sp.loCuts[xB-1] + 1
+			}
+
+			sp.hiCuts[xB-1] = sp.loCuts[xB]
+
+			if sp.hiCuts[xB-1] >= sp.fftSize {
+				sp.hiCuts[xB-1] = sp.fftSize - 1
+			}
+
+			var diff = sp.hiCuts[xB-1] - sp.loCuts[xB-1] + 1
+
+			sp.eqBins[xB-1] = math.Log2(float64(xB+2)) / float64(diff)
+		}
+	}
+
+	return bins
 }
