efe84bec65c35a71c7fbe54af8d55c8c7ead88e5
noriah reuland <code@noriah.dev>
2020-10-21 18:42:21 -0700

    more work on display.
    attempting to make a better histogram

--- dsp/monstercat.go
+++ dsp/monstercat.go
@@ -3,19 +3,27 @@ package dsp
 import "math"
 
 // Monstercat does monstercat "smoothing"
+//
 // https://github.com/karlstav/cava/blob/master/cava.c#L157
+//
+// TODO(winter): make faster (rewrite)
+//	slow and hungry as heck!
+//	lets look into SIMD
+//
+//  lets look
 func Monstercat(bins []float64, count int, factor float64) {
 
 	// "pow is probably doing that same logarithm in every call, so you're
 	//  extracting out half the work"
-	var lf = math.Log(factor)
+	var vFactP = math.Log(factor)
 
 	for xBin := 1; xBin < count; xBin++ {
 
 		for xTrgt := 0; xTrgt < count; xTrgt++ {
 
 			if xBin != xTrgt {
-				var tmp = bins[xBin] / math.Exp(lf*math.Abs(float64(xBin-xTrgt)))
+				var tmp = bins[xBin]
+				tmp /= math.Exp(vFactP * math.Abs(float64(xBin-xTrgt)))
 
 				if tmp > bins[xTrgt] {
 					bins[xTrgt] = tmp
---
+++ graphic/draw_up.go
@@ -0,0 +1,76 @@
+package graphic
+
+import "github.com/nsf/termbox-go"
+
+// DrawUp takes data and draws
+func drawUp(bins [][]float64, count int, cfg Config, scale float64) error {
+	var cWidth, cHeight = termbox.Size()
+
+	var vHeight = cHeight - cfg.BaseThick
+	if vHeight < 0 {
+		vHeight = 0
+	}
+
+	scale = float64(vHeight) / scale
+
+	var cPaddedWidth = (cfg.BinWidth * count * len(bins)) - cfg.SpaceWidth
+
+	if cPaddedWidth > cWidth || cPaddedWidth < 0 {
+		cPaddedWidth = cWidth
+	}
+
+	var xCol = (cWidth - cPaddedWidth) / 2
+
+	var delta = 1
+	var xBin int
+	// var xBin = count - 1
+
+	for xCh := range bins {
+		var stop, top = stopAndTop(bins[xCh][xBin]*scale, vHeight, true)
+
+		var lCol = xCol + cfg.BarWidth
+		var lColMax = xCol + (cfg.BinWidth * count) - cfg.SpaceWidth
+
+		for {
+			if xCol >= lCol {
+				if xCol >= lColMax {
+					break
+				}
+
+				if xBin += delta; xBin >= count || xBin < 0 {
+					break
+				}
+
+				stop, top = stopAndTop(bins[xCh][xBin]*scale, vHeight, true)
+
+				xCol += cfg.SpaceWidth
+				lCol = xCol + cfg.BarWidth
+			}
+
+			var xRow = cHeight
+
+			for xRow >= vHeight {
+				termbox.SetCell(xCol, xRow, BarRune, StyleCenter, StyleDefaultBack)
+
+				xRow--
+			}
+
+			for xRow >= stop {
+				termbox.SetCell(xCol, xRow, BarRune, StyleDefault, StyleDefaultBack)
+
+				xRow--
+			}
+
+			if top > BarRuneR {
+				termbox.SetCell(xCol, xRow, top, StyleDefault, StyleDefaultBack)
+			}
+
+			xCol++
+		}
+
+		xCol += cfg.SpaceWidth
+		delta = -delta
+	}
+
+	return nil
+}
